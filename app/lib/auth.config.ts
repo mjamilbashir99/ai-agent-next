efb0fcf1da725d0098bff9c76d2a44b59b64e0c5
SufiDevelope <muhammadsufyan112011@gmail.com>
2025-02-28 01:49:23 -0800

    fixed auth login issue

--- app/lib/auth.config.ts
+++ app/lib/auth.config.ts
@@ -1,26 +1,9 @@
-import type { AuthOptions, DefaultSession } from "next-auth";
-import Twitter from "next-auth/providers/twitter";
-import { JWT } from "next-auth/jwt";
-
-interface ExtendedToken extends JWT {
-  accessToken?: string;
-  email?: string;
-}
-
-interface ExtendedSession extends DefaultSession {
-  accessToken?: string;
-  user: {
-    name?: string | null;
-    email?: string | null;
-    image?: string | null;
-  }
-}
+import type { AuthOptions } from "next-auth";
+import TwitterProvider from "next-auth/providers/twitter";
 
 export const authConfig: AuthOptions = {
   providers: [
-    Twitter({
-      // clientId: "QnE5UGtyZHBSbzV4ZGNqLWpqV1o6MTpjaQ",
-      // clientSecret: "18oRqU7rXotoNEmlV6d-5JKwFWoyA75tHSh9DgeccEeMl6PEQw",
+    TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID!,
       clientSecret: process.env.TWITTER_CLIENT_SECRET!,
       version: "2.0",
@@ -31,31 +14,19 @@ export const authConfig: AuthOptions = {
       },
     }),
   ],
-  secret: "f13b18ca71e997bfcf1507ec27da005fa2f0d952b9f87be16fdce32e4e1a8579",
+  secret: process.env.NEXTAUTH_SECRET,
   debug: true,
   callbacks: {
-    async jwt({ token, account, user, profile }) {
-      // Initial sign in
-      if (account && user) {
-        console.log("JWT Callback", { account, user, profile }); // Debug log
-        return {
-          ...token,
-          accessToken: account.access_token,
-          email: user.email || profile?.email,
-          username: user.name,
-        };
+    async jwt({ token, account, user }) {
+      if (account) {
+        console.log("JWT Callback", { account, user });
+        token.accessToken = account.access_token;
       }
       return token;
     },
     async session({ session, token }) {
-      console.log("Session Callback", { session, token }); // Debug log
-      
-      // Send properties to the client
+      console.log("Session Callback", { session, token });
       session.accessToken = token.accessToken;
-      if (token.email) {
-        session.user.email = token.email;
-      }
-      
       return session;
     },
   },
